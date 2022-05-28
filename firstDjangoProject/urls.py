6eaf217fcb58a4d440a913593d8789662916c1f3
asakundwi <mavhasa17@gmail.com>
2022-05-28 12:01:16 +0200

    created and initialized new app 'tasks'

--- firstDjangoProject/urls.py
+++ firstDjangoProject/urls.py
@@ -19,5 +19,6 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/', include("hello.urls")),
-    path('christmas', include("christmas.urls"))
+    path('christmas/', include("christmas.urls")),
+    path('tasks/', include("tasks.urls"))
 ]
