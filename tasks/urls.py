3123fd3c1f29cb1e539d2fa77e6af4664d511226
asakundwi <mavhasa17@gmail.com>
2022-05-28 14:08:59 +0200

    minor changes and fixes

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,6 +2,5 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path("", views.index, name = "index"),
-    path("<str:name>", views.greet, name = "index")
+    path("", views.index, name = "index")
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -2,11 +2,10 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+tasks = ["foo", "bar", "faz"]
+
 def index(request):
-    return HttpResponse(f"Hello there")
-
-
-def greet(request, name:str):
     return render(request, "tasks/index.html", {
-        "name" : name 
+        "tasks" : tasks
     })
