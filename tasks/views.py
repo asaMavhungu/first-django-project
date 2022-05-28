b1bb636d9525d9412b41b64e5e368d3072f8125d
asakundwi <mavhasa17@gmail.com>
2022-05-28 13:48:19 +0200

    made initial html and css for tasks

--- tasks/views.py
+++ tasks/views.py
@@ -2,5 +2,11 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def index(reqest):
-    return HttpResponse("Hello There")
+def index(request):
+    return HttpResponse(f"Hello there")
+
+
+def greet(request, name:str):
+    return render(request, "tasks/index.html", {
+        "name" : name 
+    })
