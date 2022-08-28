760d1ea7e1d11b39e68018a115db28553fe33ecc
Asakundwi <asakundwi@pop-os.localdomain>
2022-08-28 17:38:47 +0200

    l

--- firstDjangoProject/settings.py
+++ firstDjangoProject/settings.py
@@ -32,7 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'hello',
-    'bDay',
+    'christmas',
+    'tasks',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
